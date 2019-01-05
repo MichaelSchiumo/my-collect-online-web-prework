4905211356ffc93b255bb0a631c0304f5fe3a803
Michael J. Schiumo <mschiumo18@gmail.com>
2019-01-05 01:23:09 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,10 +3,10 @@ def my_collect(collection)
 i = 0 
 new_collection = []
 while i < collection.length 
- new_array << yield(collection[i])
+ new_collection << yield(collection[i])
   i += 1
 end  
-new_array
+new_collection
 end
 
 
