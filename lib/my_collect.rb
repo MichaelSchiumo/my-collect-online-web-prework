5e2ce14af11ff821733cadaa19539338359206a4
Michael J. Schiumo <mschiumo18@gmail.com>
2019-01-05 01:21:12 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,12 @@
+def my_collect(collection)
+new_array = []
+i = 0 
 
+while i < collection.length 
+  yield(collection[i])
+  i = i + 1
+  new_array << yield(collection[i])
+end  
+new_array
+end
 
